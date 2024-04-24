7b4e47f2bedd614cd84436b9f6aa6e62a72e29d0
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-04-24 13:34:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
 
+#include "get_next_line.h"
+
+int	main(void)
+{
+	int		fd; // descriptor de archivo
+	char	*line; // Variable para almacenar la linea leida
+	//Abre un arcivo para lectura
+	fd = open("archivo.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Error al abrir el archivo");
+		return (1);
+	}
+	//lee lineas del archivo hasta que se alcanza el final
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		printf("%s\n", line); //Imprime la linea leida 
+		free(line); //libera la memoria asignada por get_next_line 
+		line = get_next_line(fd); // lee la siguiente línea
+	}
+	//cierra el archivo
+	close(fd);
+	return (0);
+}
