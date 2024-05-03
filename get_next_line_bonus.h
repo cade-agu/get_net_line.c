34f55fab52182db36349ce0fb095b0e6bd19d1cd
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-05-03 13:41:34 +0200

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,4 +1,5 @@
 
+
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
@@ -7,8 +8,8 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
 # endif
 
 char	*get_next_line(int fd);
@@ -20,3 +21,4 @@ char	*ft_getline(char *fullStr);
 char	*ft_getrest(char *fullStr);
 
 #endif
+
