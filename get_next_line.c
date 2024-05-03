50b29b08b45ed1254264e7a87bc84949afeaa7c9
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-05-03 13:43:04 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,71 +1,52 @@
 
-#include "get_next_line.h"
+include "get_next_line.h"
 
-char	*get_next_line(int fd)
-{
-	static char	*full_str = NULL;
-	char		*line;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
-	full_str = read_function(fd, full_str);
-	if (!full_str)
-		return (NULL);
-	line = ft_getline(full_str);
-	full_str = ft_getrest(full_str);
-	return (line);
-}
-
-char	*read_function(int fd, char *str)
+char	*ft_read_function(int fd, char *aux_line)
 {
 	char	*tmp;
-	int		bytes;
+	int		read_bytes;
+	char	*save_aux;
 
-	tmp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	tmp = ft_calloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!tmp)
 		return (NULL);
-	bytes = 1;
-	while (!ft_strchr(str, '\n') && (bytes != 0))
+	read_bytes = 1;
+	while (!ft_strchr(str, '\n') && read_bytes > 0)
 	{
-		bytes = read(fd, tmp, BUFFER_SIZE);
-		if (bytes == -1)
+		read_bytes = read(fd, tmp, BUFFER_SIZE);
+		if (read_bytes == -1)
 		{
-			free(tmp);
+			free (aux_line);
+			aux_line = NULL;
 			return (NULL);
 		}
-		tmp[bytes] = '\0';
-		str = ft_strjoin(str, tmp);
+		save_aux = aux_line;
+		aux_line = ft_strjoin(save_aux, tmp, read_bytes);
 	}
 	free(tmp);
-	return (str);
+	if (ft_strlen(aux_line) == 0)
+		return (free(aux_line), NULL);
+	return (aux_line);
 }
 
-char	*ft_getline(char *full_str)
+char	*ft_getline(char *aux_line)
 {
 	int		i;
-	char	*line;
+	int		j;
+	char	*get_line;
 
 	i = 0;
-	if (!full_str[i])
+	while (aux_line[i] != '\0' && aux_line[i] != '\n')
+		i++;
+	get_line = ft_calloc ((i + 1 + (aux_line[i]) == '\n'), sizeof (char));
+	if (!get_line)
 		return (NULL);
-	while (full_str[i] && full_str[i] != '\n')
-		i++;
-	line = (char *)malloc(sizeof(char) * (i + 2));
-	if (!line)
-		return (NULL);
-	i = 0;
-	while (full_str[i] && full_str[i] != '\n')
+	j = 0;
+	while (aux_line[j] != '\0' && aux_line[j] != '\n')
 	{
-		line[i] = full_str[i];
-		i++;
+		get_line[j] = '\0';
+		return (get_line);
 	}
-	if (full_str[i] == '\n')
-	{
-		line[i] = full_str[i];
-		i++;
-	}
-	line[i] = '\0';
-	return (line);
 }
 
 char	*ft_getrest(char *full_str)
@@ -75,22 +56,40 @@ char	*ft_getrest(char *full_str)
 	char	*restof;
 
 	i = 0;
-	j = 0;
-	while (full_str[i] && full_str[i] != '\n')
+	while (full_str[i] != '\0' && full_str[i] != '\n')
 		i++;
-	if (!full_str[i])
+	if (ft_strlen(full_str) - i <= 0)
+	{
+		free(full_str);
+		full_str = NULL;
 		return (NULL);
-	restof = (char *)malloc(sizeof(char) * (ft_strlen(full_str) - i));
+	}
+	restof = ft_calloc(ft_strlen(full_str) - i + 1, sizeof(char));
 	if (!restof)
 		return (NULL);
 	i++;
-	while (full_str[i])
-	{
-		restof[j] = full_str[i];
-		i++;
-		j++;
-	}
-	restof[j] = '\0';
+	while (full_str[i] != '\0')
+		restof[j++] = full_str[i++];
 	free(full_str);
+	full_str = NULL;
 	return (restof);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*full_str;
+	char		*final_line;
+
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
+	{
+		free(full_str);
+		full_str = NULL;
+		return (NULL);
+	}
+	full_str = ft_read_function(fd, full_str);
+	if (!full_str)
+		return (NULL);
+	final_line = ft_getline(full_str);
+	full_str = ft_getrest(full_str);
+	return (final_line);
 }
