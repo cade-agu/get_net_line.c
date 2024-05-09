55e3d0b779a487fa88ea9a212fa0ba8f32f15baf
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-05-09 17:19:44 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -5,18 +5,21 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
+# include <string.h>
 # include <stdio.h>
+# include <fcntl.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *aux_line, char *buffer, int read_bytes);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_read_line(int fd, char *aux_line);
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strchr(char *s, int c);
-size_t	ft_strlen(char *s);
-char	*read_function(int fd, char *str);
-char	*ft_getline(char *fullStr);
-char	*ft_getrest(char *fullStr);
+char	*ft_get_line(char *aux_line);
+char	*ft_clean_line(char *aux_line);
+
 #endif
