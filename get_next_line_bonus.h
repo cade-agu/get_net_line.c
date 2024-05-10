c865c27c8e38ad65835fd64d936608396e22707c
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-05-10 13:33:01 +0200

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,5 +1,4 @@
 
-
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
@@ -7,18 +6,19 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <string.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strchr(char *s, int c);
-size_t	ft_strlen(char *s);
-char	*read_function(int fd, char *str);
-char	*ft_getline(char *fullStr);
-char	*ft_getrest(char *fullStr);
+char	*ft_strjoin(char *aux_line, char *buffer, int read_bytes);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_read_function(int fd, char *aux_line);
+char	*ft_get_line(char *aux_line);
+char	*ft_getrest(char *aux_line);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
-
